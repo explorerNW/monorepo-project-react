41baf6312eb50766fecb14ba7fd505e0f994814b
NieWang <superexplorernw@163.com>
2024-12-25 23:00:13 +0800

    feat(001): init apps/front-end-dashboard, use @me/ui input element

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['src/index.ts'],
     target: 'ES2020',
+    // dts: true,
     shims: true,
     minify: 'terser',
     format: ['esm'],
