1d8a48fb28fd70b4556e1865d5049fc1feb55c7e
NieWang <superexplorernw@163.com>
2025-01-16 22:52:49 +0800

    feat(006): myCli init

--- packages/cli/lib/base.ts
+++ packages/cli/lib/base.ts
@@ -1,15 +1,62 @@
 import { program } from 'commander';
+import picocolors from 'picocolors';
+import prompts from 'prompts';
+
 import { version } from '../package.json';
 
-program.name('myCli').description('cli').version(version);
+const init = async () => {
+  program.name('myCli').description('cli').version(version);
 
-program
-  .command('init')
-  .arguments('<name>')
-  .option('-t, --template <template>', 'template')
-  .description('init project')
-  .action((name, option) => {
-    console.log('init project', name, option);
-  });
+  program
+    .command('init')
+    .argument('<name>', 'init project')
+    .option('-t, --template <template>', 'template')
+    .action((name, option) => {
+      console.log(picocolors.green(`init project', ${name}, ${option}`));
+    });
 
-program.parse();
+  const result = await prompts([
+    {
+      type: 'text',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'number',
+      name: 'age',
+      message: 'How old are you?',
+      initial: 18,
+    },
+    {
+      type: 'confirm',
+      name: 'cool',
+      message: 'Are you cool?',
+    },
+    {
+      type: 'select',
+      name: 'color',
+      message: 'Pick a color',
+      choices: [
+        { title: 'Red', value: '#ff0000' },
+        { title: 'Green', value: '#00ff00' },
+        { title: 'Blue', value: '#0000ff' },
+      ],
+    },
+    {
+      type: 'text',
+      name: 'dir',
+      message: 'Where to put the files?',
+      initial: __dirname.split('/').pop(),
+    },
+  ]);
+
+  console.log(
+    picocolors.green(
+      `Hello, ${result.name}!, ${result.age}, ${result.cool}, ${result.color}, ${result.dir}`
+    )
+  );
+
+  program.parse();
+};
+
+init();
